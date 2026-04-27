127d6b6d219ae0460be702e6fe8419f9999c4633
Francesco-Zucco <zuccoqwe@gmail.com>
2026-04-27 14:50:23 +0300

    added on the homescreen the weather for the user location instead of showing nothing

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDragScroll } from "./hooks/useDragScroll";
 
 export default function Home() {
-  const [userInput, setUserInput] = useState("bucharest");
+  const [userInput, setUserInput] = useState("");
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const dragScrollRef = useDragScroll();
 
-  const getWeather = async () => {
-    if (!userInput.trim()) return;
+  const getWeather = async (query) => {
+    if (!query.trim()) return;
     setLoading(true);
     setError(null);
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${userInput}&days=1`;
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}&days=2`;
     const response = await fetch(url);
     const data = await response.json();
     setLoading(false);
@@ -25,20 +25,24 @@ export default function Home() {
     }
     setWeather(data);
     console.log(data);
+    console.log("auto:ip");
   };
-  const hours = weather ? weather.forecast.forecastday[0].hour : [];
+
+  useEffect(() => {
+    getWeather("auto:ip");
+  }, []);
+  const todayHours = weather ? weather.forecast.forecastday[0].hour : [];
+  const tomorrowHours = weather ? weather.forecast.forecastday[1].hour : [];
+
   const localTime = weather?.location?.localtime;
   const currentHour = localTime ? localTime.split(" ")[1].split(":")[0] : null;
 
-  const currentIndex = hours.findIndex((hour) => {
+  const currentIndex = todayHours.findIndex((hour) => {
     const hourNumber = hour.time.split(" ")[1].split(":")[0];
     return hourNumber === currentHour;
   });
 
-  const orderedHours = [
-    ...hours.slice(currentIndex),
-    ...hours.slice(0, currentIndex),
-  ];
+  const orderedHours = [...todayHours.slice(currentIndex), ...tomorrowHours];
   console.log(currentHour);
   let video = "/cloudy.mp4";
 
@@ -62,7 +66,7 @@ export default function Home() {
 
   return (
     <div
-      className={`relative h-[100dvh] w-full flex flex-col  gap-4 ${weather && weather.current.condition.text.includes("night") ? "bg-black" : ""} `}
+      className={`relative h-[100dvh] w-full flex flex-col  gap-4 ${weather && weather.current.condition.text.includes("night") ? "bg-black" : ""} select-none `}
     >
       <video
         playsInline
@@ -77,14 +81,19 @@ export default function Home() {
       <div className="max-w-[800px] h-full w-full z-50 px-4 flex flex-col  gap-4 mx-auto p-4">
         <nav className="w-full h-[100px] flex items-center justify-center gap-2">
           <input
-            onKeyDown={(e) => e.key === "Enter" && getWeather()}
+            onKeyDown={(e) => e.key === "Enter" && getWeather(userInput)}
             placeholder="Search by city name"
             type="text"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
-            className="border text-base"
+            className="flex-1 bg-transparent px-3 border border-white/25  rounded-full py-2 text-white placeholder:text-white/60 outline-none text-base"
           />
-          <button onClick={getWeather}>Search</button>
+          <button
+            className="rounded-full bg-white/20 px-5 py-2 text-white font-medium border border-white/25 hover:bg-white/30 active:scale-95 transition-all duration-150 select-none"
+            onClick={() => getWeather(userInput)}
+          >
+            Search
+          </button>
         </nav>
         <div>
           {loading ? (
@@ -94,14 +103,14 @@ export default function Home() {
           ) : (
             weather && (
               <div className="w-full h-full flex flex-col items-center justify-center ">
-                <h2 className="text-white font-light text-2xl">
+                <h2 className="text-white font-light text-2xl select-none pointer-events-none">
                   {weather.location.name}
                 </h2>
-                <p className="text-white text-6xl font-extralight">
+                <p className="text-white text-6xl font-extralight select-none pointer-events-none">
                   {weather.current.temp_c}°C
                 </p>
                 <div className="flex items-center ">
-                  <p className="text-white/90 px-2  text-nowrap">
+                  <p className="text-white/90 px-2  text-nowrap select-none pointer-events-none">
                     {weather.current.condition.text}
                   </p>
                   {/* <Image
@@ -112,11 +121,11 @@ export default function Home() {
                   /> */}
                 </div>
                 <div className="flex gap-2 text-white">
-                  <p>
+                  <p className="select-none pointer-events-none">
                     H:{" "}
                     {Math.floor(weather.forecast.forecastday[0].day.maxtemp_c)}°
                   </p>
-                  <p>
+                  <p className="select-none pointer-events-none">
                     L:{" "}
                     {Math.floor(weather.forecast.forecastday[0].day.mintemp_c)}°
                   </p>
@@ -127,21 +136,21 @@ export default function Home() {
         </div>
         <div
           ref={dragScrollRef}
-          className=" flex drag-scroll  overflow-x-auto gap-3 cursor-pointer  [&::-webkit-scrollbar]:hidden
+          className=" flex drag-scroll  overflow-x-auto gap-3 cursor-pointer   [&::-webkit-scrollbar]:hidden
     [-ms-overflow-style:none]
     [scrollbar-width:none]  rounded-2xl bg-white/5  backdrop-blur-xs "
         >
           {loading ? (
-            <p>Loading...</p>
+            <p className="select-none pointer-events-none">Loading...</p>
           ) : error ? (
-            <p>Error: {error}</p>
+            <p className="select-none pointer-events-none">Error: {error}</p>
           ) : (
             orderedHours.map((hour, index) => (
               <div
                 key={index}
-                className="min-w-[70px] flex-shrink-0 rounded-2xl p-3 flex flex-col items-center gap-1 scroll-smooth"
+                className="min-w-[70px] flex-shrink-0 rounded-2xl p-3 flex flex-col items-center gap-1 scroll-smooth select-none pointer-events-none"
               >
-                <p className="text-white">
+                <p className="text-white select-none pointer-events-none text-[14px]">
                   {index === 0 ? "Now" : hour.time.split(" ")[1].split(":")[0]}
                 </p>
                 <Image
@@ -152,7 +161,9 @@ export default function Home() {
                   width={40}
                   height={40}
                 />
-                <p className="text-white">{Math.round(hour.temp_c)}°</p>
+                <p className="text-white select-none pointer-events-none text-base">
+                  {Math.round(hour.temp_c)}°
+                </p>
               </div>
             ))
           )}
